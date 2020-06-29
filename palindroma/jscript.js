0638ba6685f6028e7a9a73eafb2a81e5617ebffc
Umberto <umbertopellegrino96@gmail.com>
2020-06-29 15:23:16 +0200

    Commenti palindroma

--- palindroma/jscript.js
+++ palindroma/jscript.js
@@ -1,6 +1,9 @@
+// Chiedere all’utente di inserire una parola
 var parolaUtente = prompt("inserisci la parola da controllare:");
 console.log(checkPalindroma(parolaUtente));
 
+
+//Creare una funzione per capire se la parola inserita è palindroma
 // function
 function checkPalindroma (parola) {
   var palindroma = parola.split('').reverse().join('');
